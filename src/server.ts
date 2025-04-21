b7df63ee2d4055b57241de8faaa67f432049b61d
Leandro Reis <developer.reis@gmail.com>
2025-04-21 06:30:32 +0100

    amplify changes

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-const browserDistFolder = resolve(serverDistFolder, '../browser');
+const browserDistFolder = resolve(serverDistFolder, '../../static');
 
 const app = express();
 const angularApp = new AngularNodeAppEngine();
@@ -54,7 +54,7 @@ app.use('/**', (req, res, next) => {
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
 if (isMainModule(import.meta.url)) {
-  const port = process.env['PORT'] || 4000;
+  const port = process.env['PORT'] || 3000;
   app.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
