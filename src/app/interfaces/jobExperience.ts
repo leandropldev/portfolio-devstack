eb6e2bbadbf1b4816f4ed7539ab15318e0d4855b
Leandro Reis <developer.reis@gmail.com>
2025-04-21 11:11:21 +0100

    input for cards

---
+++ src/app/interfaces/jobExperience.ts
@@ -0,0 +1,9 @@
+export interface JobExperince {
+    position: string,
+    company: string,
+    companyCountry: string,
+    dateBegin: string,
+    dateEnd: string,
+    activities: string[],
+    stacks: string[ ]
+}
--- src/app/pages/home/career/career.component.ts
+++ src/app/pages/home/career/career.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CardComponent } from "./card/card.component";
+import { JobExperince } from '../../../interfaces/jobExperience';
 
 @Component({
   selector: 'app-career',
@@ -9,4 +10,58 @@ import { CardComponent } from "./card/card.component";
 })
 export class CareerComponent {
 
+  cards: JobExperince[] = [
+    {
+      position: "FullStack Developer",
+      company: "Wafx Portugal",
+      companyCountry: "pt",
+      dateBegin: "Setembro 2022",
+      dateEnd: "Present",
+      activities: [
+        "Incorporated third-party APIs into the web application for data exchange and real-time updates",
+        "Utilized Java frameworks such as Spring, Hibernate and Angular",
+        "Implemented a CI/CD pipeline for testing and deployment",
+        "AWS resources creation and maintenance (EC2, ELB, Gateway)"
+      ],
+      stacks: [
+        "Spring Boot",
+        "Angular",
+        "Argos CD",
+        "Docker",
+        "PostgreSQL",
+        "Git Corp",
+        "Scrum",
+        "Mockito",
+        "Junit",
+        "Jest",
+        "AWS"
+      ]
+    },
+    {
+      position: "FullStack bronha",
+      company: "Wafx Portugal",
+      companyCountry: "br",
+      dateBegin: "Setembro 2022",
+      dateEnd: "Present",
+      activities: [
+        "Incorporated third-party APIs into the web application for data exchange and real-time updates",
+        "Utilized Java frameworks such as Spring, Hibernate and Angular",
+        "Implemented a CI/CD pipeline for testing and deployment",
+        "AWS resources creation and maintenance (EC2, ELB, Gateway)"
+      ],
+      stacks: [
+        "Spring Boot",
+        "Angular",
+        "Argos CD",
+        "Docker",
+        "PostgreSQL",
+        "Git Corp",
+        "Scrum",
+        "Mockito",
+        "Junit",
+        "Jest",
+        "AWS"
+      ]
+    }
+  ]
 }
